723859d085a58961eb6aabcd75bde6f650b85866
Akshay Kale <kaleakshay089@gmail.com>
2025-06-29 22:55:38 +0530

    Updated metro.config.js for handling the auth error

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './app/globals.css' }); 
+// Firebase Auth + Hermes Fix
+config.resolver.sourceExts.push("cjs");
+config.resolver.unstable_enablePackageExports = false;
+
+// NativeWind CSS
+module.exports = withNativeWind(config, {
+  input: "./app/globals.css",
+});
