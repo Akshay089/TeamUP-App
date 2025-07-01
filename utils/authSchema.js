b0fadc74917b831af1bf13f2e7d5af0ead5421f2
Akshay Kale <kaleakshay089@gmail.com>
2025-07-02 03:17:06 +0530

    Updated authSchema(Name is not required)

--- utils/authSchema.js
+++ utils/authSchema.js
@@ -1,5 +1,8 @@
 import * as Yup from "yup";
 const validationSchema=Yup.object().shape({
+    // Only require fullName for signup, not for signin
+    fullName: Yup.string(),
+
     email:Yup.string().required("Email is required").email("Invalid email format"),
 
     password: Yup.string().required("Password is required").min(6,"Password mus be of 6 characters long"),
